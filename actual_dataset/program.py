3b6c052ef8c46f04b97ed69f1ac277ac1a3c3494
eqan <eqan.ahmad123@gmail.com>
2022-05-26 19:40:55 +0500

    Initial Commit

---
+++ actual_dataset/program.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import os
+import csv
+
+courses, rooms, studentCourses, studentNames, teachers = [], [], [], [], []
+countCourseRegisteredStudents = {}
+
+def importFiles():
+    global courses, rooms, studentCourses, studentNames, teachers
+    courses = pd.read_csv(r'./courses.csv')
+    rooms = pd.read_csv(r'./rooms.csv')
+    studentCourses = pd.read_csv(r'./studentCourses.csv')
+    studentNames = pd.read_csv(r'./studentNames.csv')
+    teachers = pd.read_csv(r'./teachers.csv')
+
+def countStudentsInCourse():
+    global studentCourses, courses, countCourseRegisteredStudents
+    for i, j, studentNameSC, courseCodeSC in studentCourses.itertuples():
+        for i, courseCodeC, courseNameC in courses.itertuples():
+            if(courseCodeC == courseCodeSC):
+                if courseCodeC not in countCourseRegisteredStudents:
+                    countCourseRegisteredStudents[courseCodeC] = 0
+                else:
+                    countCourseRegisteredStudents[courseCodeC] +=1
+
+importFiles()
+countStudentsInCourse()
+print(countCourseRegisteredStudents)
