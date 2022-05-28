5eaeb15e3caaff8dbe150e6a3004441b17282b70
eqan <eqan.ahmad123@gmail.com>
2022-05-28 21:16:34 +0500

    Developing Random Solution

--- program.py
+++ program.py
@@ -2,9 +2,14 @@ import pandas as pd
 import numpy as np
 import os
 import csv
+import random
 
 courses, rooms, studentCourses, studentNames, teachers = [], [], [], [], []
 countCourseRegisteredStudents = {}
+studentsLimit  = 0
+currentDate = [0,0] #Hour Shift , Day 
+availableRooms = []
+availableTeachers = []
 
 fileDir = './actual_dataset/'
 
@@ -26,6 +31,63 @@ def countStudentsInCourse():
                 else:
                     countCourseRegisteredStudents[courseCodeC] +=1
 
+def calculateStudentsLimitInRooms():
+    global studentsLimit
+    for i, roomNumber, capacity in rooms.itertuples():
+        studentsLimit += int(capacity)
+
+def returnCourseRegisteredStudentsAsList():
+    return list(countCourseRegisteredStudents.items())
+
+def randomlyPickExamAndReturnListOfExams(entry_list):
+    exam = random.choice(entry_list)
+    entry_list.remove(exam)
+    return exam, entry_list
+
+def resetDataForNextDay():
+    return 1
+
+def returnTimeAndDate():
+    assignTimeStamp = 0
+    assignDate = currentDate[1]
+    if(currentDate[0] == 0):
+        assignTimeStamp = 9
+        currentDate[0] = 1
+    else:
+        assignTimeStamp = 2
+        currentDate[0] = 0
+        currentDate[1] += 1
+        resetDataForNextDay()
+
+    return assignTimeStamp, assignDate
+
+def returnTeacher():
+    return 1
+
+def returnRoom():
+    return 1
+
+def assignRoomTeacherAndTime(result):
+    time, date = returnTimeAndDate()
+    return 1
+
+def createCombination(exam, remainingExamsList):
+    results = []
+    totalSum = exam[1]
+    results.append(exam)
+    random.shuffle(remainingExamsList)
+    for exam in remainingExamsList:
+        if( (totalSum+exam[1]) <= studentsLimit):
+            totalSum += exam[1]
+            results.append(exam)
+            remainingExamsList.remove(exam)
+
 importFiles()
 countStudentsInCourse()
-print(countCourseRegisteredStudents)
+calculateStudentsLimitInRooms()
+exam, remainingExamsList = randomlyPickExamAndReturnListOfExams(returnCourseRegisteredStudentsAsList())
+# print(exam)
+# print(remainingExamsList)
+# exam = ('JK123', 23)
+# remainingExamsList = [('Pk', 30), ('LI', 50),  ('MNA', 20)]
+createCombination(exam, remainingExamsList)
