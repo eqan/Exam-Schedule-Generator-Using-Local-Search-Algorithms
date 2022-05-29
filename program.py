459a2916fd769ee56befc3cc4746d7a4cc7f5aeb
eqan <eqan.ahmad123@gmail.com>
2022-05-29 16:11:12 +0500

    Still resolving random solution

--- program.py
+++ program.py
@@ -105,34 +105,76 @@ def convertEnrolledStudentsToRooms(result):
             partialRooms.append((courseCode, floatValue))
     return partialRooms, fullRooms
 
-def insertForFullValues(fullRooms, table, date, time, teachers, i):
+def insertForFullValues(fullRooms, table, i):
     for courseCode, capacity in fullRooms:
         for roomNumber in range(int(capacity)):
-            table.append((date, time, courseCode, teachers[i], i+1))
+            table.append((courseCode, i+1))
             i+=1
-    return fullRooms, table, date, time, teachers, i
+    return fullRooms, table, i
 
-def insertForPartialValues(sum , partialRooms, table, date, time, teachers, i):
+def insertForPartialValues(partialRooms, table, i):
+    sum = 0
     partialCourseList = []
     for courseCode, capacity in partialRooms:
-        if (courseCode, capacity) in partialRooms:
-            partialRooms.remove((courseCode,capacity))
-        else:
-            print("Not Found" + str((courseCode, capacity)))
-        sum+=capacity
-        if(sum >=0.85 and sum <= 1):
-            for courseCode, capacity in partialCourseList:
-                table.append((date, time, courseCode, teachers[i], i+1))
-            i+=1
-            partialCourseList = []
-            sum = 1 - sum
-        elif(sum > 1):
-            for courseCode, capacity in partialCourseList:
-                table.append((date, time, courseCode, teachers[i], i+1))
+        if((sum+capacity >=1)): # For Multiple Class Rooms
+            sum = 0
+            if(len(partialCourseList) > 0):
                 i+=1
+                for courseCode2, capacity2 in partialCourseList:
+                    table.append((courseCode2, i+1))
+            i+=1
+            if(capacity > 0.85):
+                table.append((courseCode, i+1))
             partialCourseList = []
+        elif((sum+capacity >=0.9) and (sum+capacity) <= 1): # For just a single classroom
+            sum = 0
+            i+=1
+            for courseCode2, capacity2 in partialCourseList:
+                table.append((courseCode2, i+1))
+            partialCourseList= []
+
         partialCourseList.append((courseCode, capacity))
-    return sum , partialRooms, table, date, time, teachers, i
+        sum+=capacity
+    if(len(partialCourseList) > 0):
+        for courseCode2, capacity2 in partialCourseList:
+            table.append((courseCode2, i+1))
+    return sum , partialRooms, table, i
+
+
+    # partialCourseList = []
+    # for courseCode, capacity in partialRooms:
+    #     partialRooms.remove((courseCode, capacity))
+    #     sum+=capacity
+    #     if(sum >=0.9 and sum <= 1): # For just a single classroom
+    #         for courseCode, capacity in partialCourseList:
+    #             table.append((courseCode, i+1))
+    #         partialCourseList = []
+    #         sum = 0
+    #         i+=1
+    #     elif(sum > 1): # For assigning multiple classrooms
+    #         partialCourseList.sort(key=lambda s: s[1], reverse=True)
+    #         for courseCode, capacity in partialCourseList:
+    #             if(sum > 1):
+    #                 table.append((courseCode, i+1))
+    #                 sum -=capacity
+    #                 i+=1
+    #             else:
+    #                 table.append((courseCode, i+1))
+    #         partialCourseList = []
+    #         sum = 0
+    #         i+=1
+    #     partialCourseList.append((courseCode, capacity))
+
+    # print("Partial Filled Rooms: " + str(partialRooms))
+    # # print("Resultant Table"+ str(table))
+    # # print(partialRooms)
+    # if(len(partialRooms) > 0):
+    #     insertForPartialValues(sum, partialRooms, table, i)
+    # else:
+    #     for courseCode, capacity in partialCourseList:
+    #         table.append((courseCode, i+1))
+
+    # return sum , partialRooms, table, i
 
 
 def assignRoomTeacherAndTimeForAShift(result):
@@ -144,35 +186,38 @@ def assignRoomTeacherAndTimeForAShift(result):
     partialRooms.sort(key=lambda s: s[1])
     fullRooms.sort(key=lambda s: s[1])
     # print(teachers)
-    # print("Fully Filled Rooms: " + str(fullRooms))
-    # print("Partial Filled Rooms: " + str(partialRooms))
+    print("Fully Filled Rooms: " + str(fullRooms))
+    print("Partial Filled Rooms: " + str(partialRooms))
     i = 0
     # For Full Rooms
-    fullRooms, table, date, time, teachers, i = insertForFullValues(fullRooms, table, date, time, teachers, i)
+    fullRooms, table, i = insertForFullValues(fullRooms, table, i)
     # For Half or less capacity rooms
     # print(teachers)
     # print("Partial Filled Rooms: " + str(partialRooms))
-    sum , partialRooms, table, date, time, teachers, i = insertForPartialValues(0, partialRooms, table, date, time, teachers, i)
-    while(len(partialRooms) > 0):
-        # print("Partial Filled Rooms: " + str(partialRooms))
-        try:
-            if(len(partialRooms) == 1):
-                for courseCode, capacity in partialRooms:
-                    if(sum > 1):
-                        table.append((date, time, courseCode, teachers[i], i+1))
-                        table.append((date, time, courseCode, teachers[i+1], i+2))
-                        i+=2
-                    else:
-                        table.append((date, time, courseCode, teachers[i], i+1))
-                    partialRooms.remove((courseCode,capacity))
-            else:
-                sum , partialRooms, table, date, time, teachers, i = insertForPartialValues(sum, partialRooms, table, date, time, teachers, i)
-        except:
-            print("Couldnt iterate over teachers")
-            i-=1
+    sum , partialRooms, table, i = insertForPartialValues(partialRooms, table, i)
+    # print("Partial Filled Rooms: " + str(partialRooms))
+    # while(len(partialRooms) > 0):
+    # sum , partialRooms, table, date, time, teachers, i = insertForPartialValues(0, partialRooms, table, date, time, teachers, i)
+    #     print("Partial Filled Rooms: " + str(partialRooms))
+    #     try:
+    #         if(len(partialRooms) == 1):
+    #             # # for courseCode, capacity in partialRooms:
+    #             # #     if(sum > 1):
+    #             #         table.append((date, time, courseCode, teachers[i], i+1))
+    #             #         table.append((date, time, courseCode, teachers[i+1], i+2))
+    #             #         i+=2
+    #             #     else:
+    #             table.append((date, time, courseCode, teachers[i], i+1))
+    #                 # partialRooms.remove((courseCode,capacity))
+    #         else:
+    #             sum , partialRooms, table, date, time, teachers, i = insertForPartialValues(sum, partialRooms, table, date, time, teachers, i)
+    #             print(table)
+    #     except:
+    #         print("Couldnt iterate over teachers")
+    #         break
+    #         # i-=1
     # print(studentsLimit)
-    print(table)
-    # print(table2)
+    print("Resultant Table"+ str(table))
 
 def createCombination(exam, remainingExamsList):
     global studentsLimit
@@ -191,8 +236,4 @@ def createCombination(exam, remainingExamsList):
 importFiles()
 setVariables()
 exam, remainingExamsList = randomlyPickExamAndReturnListOfExams(convertDictionaryToList(countCourseRegisteredStudents))
-# print(exam)
-# print(remainingExamsList)
-# exam = ('JK123', 23)
-# remainingExamsList = [('Pk', 30), ('LI', 50),  ('MNA', 20)]
 createCombination(exam, remainingExamsList)
