98ba5075d9bd456d23fc1935e97dbf185bc125dc
eqan <eqan.ahmad123@gmail.com>
2022-06-02 00:26:07 +0500

    Adding comments & reorganizing files

--- program-haris.py
+++ program-haris.py
@@ -16,6 +16,27 @@ days = 14
 coursesstudents = {}
 
 
+listOfHardConstraints = [
+'• An exam will be scheduled for each course.',
+'• A student is enrolled in at least 3 courses. A student cannot give more than 1 exam \
+at a time.',
+'• Exam will not be held on weekends.',
+'• Each exam must be held between 9 am and 5 pm',
+'• Each exam must be invigilated by a teacher. A teacher cannot invigilate two \
+exams at the same time.',
+'• A teacher cannot invigilate two exams in a row.'
+]
+
+listOfSoftConstraints = [
+'• All students and teachers shall be given a break on Friday from 1-2.',
+'• A student shall not give more than 1 exam consecutively.',
+'• If a student is enrolled in a MG course and a CS course, it is preferred that \
+their MG course exam be held before their CS course exam.',
+'• Two hours of break in the week such that at least half the faculty is free in \
+one slot and the rest of the faculty is free in the other slot so the faculty \
+meetings shall be held in parts as they are now.'
+]
+
 class Exam:
     course = ''
     room = []
